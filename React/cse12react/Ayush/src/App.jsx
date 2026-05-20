4bda30218937bf51623a379c48c7a1f499f2efb0
Ayush Singh <singhneetu7687@gmail.com>
2026-05-20 23:05:55 +0530

    Experiment-7

---
+++ React/cse12react/Ayush/src/App.jsx
@@ -0,0 +1,23 @@
+// function app(){
+//   return(
+//     <div className="app">
+//       <h1>Hello World</h1>
+//       <p>This is my first react app.</p>
+//       <CSE12 />
+//     </div>
+//   );
+// }
+
+// //2nd component
+// function CSE12 () {
+//   return (
+//     <div className="CSE12">
+//       <h1>Welcome TO CSE12</h1>
+//       <p>This is second component.</p>
+//     </div>
+  
+//   );
+// }
+// export default app;
+import Student from "./student";
+
